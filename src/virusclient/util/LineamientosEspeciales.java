7f2b736cbf7d6a32b5978ded8f7d391839217c9f
Roberth <roberth123pz@gmail.com>
2020-06-19 01:10:21 -0600

    Ya es prácticamente jugable

--- src/virusclient/util/LineamientosEspeciales.java
+++ src/virusclient/util/LineamientosEspeciales.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.layout.VBox;
 import virusclient.model.Carta;
+import virusclient.model.Jugador;
+import virusclient.model.MarcoCarta;
 
 ;
 
@@ -34,10 +36,34 @@ public class LineamientosEspeciales {
     private static boolean puedeLanzarVirus(Carta cartaJugada, VBox puñoCartas) {
         List<Carta> cartaList = new ArrayList(puñoCartas.getChildren());
         Carta cartaBase = cartaList.get(0);
-        return cartaBase.getColor().equals(cartaJugada.getColor()) || "Multicolor".equals(cartaJugada.getColor());
+        long numeroMedicinas = cartaList.stream().filter(carta -> "Medicina".equals(carta.getTipo())).count();
+        return (cartaBase.getColor().equals(cartaJugada.getColor()) || "Multicolor".equals(cartaJugada.getColor())) && numeroMedicinas < 2;
     }
 
-    
+    public static List<MarcoCarta> jugarEnCampoEnemigo(VBox puñoCartas, Carta cartaJuego, Jugador jugadorRecidente, Jugador enemigo) {
+        jugadorRecidente.removerCartaDeMano(cartaJuego);
+        cartaJuego.setContainerId(Integer.valueOf(puñoCartas.getId()));
+        enemigo.recibirCartaEnemiga(cartaJuego);
+        cartaJuego.toSlowFormat();
+        puñoCartas.getChildren().add(cartaJuego);
+        List<Carta> cartaList = new ArrayList(puñoCartas.getChildren());
+        long numeroVirus = cartaList.stream().filter(carta -> "Virus".equals(carta.getTipo())).count();
+        long numeroMedicinas = cartaList.stream().filter(carta -> "Medicina".equals(carta.getTipo())).count();
+        cartaList.clear();
+        if (numeroVirus == 2) {
+            cartaList.add((Carta) puñoCartas.getChildren().remove(puñoCartas.getChildren().size() - 1));
+            cartaList.add((Carta) puñoCartas.getChildren().remove(puñoCartas.getChildren().size() - 1));
+            cartaList.add((Carta) puñoCartas.getChildren().remove(puñoCartas.getChildren().size() - 1));
+        } else if (numeroMedicinas == 1) {
+            cartaList.add((Carta) puñoCartas.getChildren().remove(puñoCartas.getChildren().size() - 1));
+            cartaList.add((Carta) puñoCartas.getChildren().remove(puñoCartas.getChildren().size() - 1));
+        }
+        enemigo.removeCartasJugadas(cartaList);
+        List<MarcoCarta> toReturn = new ArrayList();
+        cartaList.forEach(cart -> toReturn.add(new MarcoCarta(cart.getTipo(), cart.getColor(), cart.getContainerId())));
+        return toReturn;
+    }
+
     public static boolean puedeUsarMedicina(Carta cartaJugada, VBox puñoCartas) {
         List<Carta> cartaList = new ArrayList(puñoCartas.getChildren());
         long numeroMedicinas = cartaList.stream().filter(carta -> "Medicina".equals(carta.getTipo())).count();
